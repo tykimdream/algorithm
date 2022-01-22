269421a875cfa5ed89c4ff28b46b7b48ecd736a4
tykimdream@korea.ac.kr <tykimdream@korea.ac.kr>
2022-01-22 21:38:48 +0900

    20220122

--- 기초/정렬.py
+++ 기초/정렬.py
@@ -1,4 +1,59 @@
-# 2751 수 정렬하기2
+############ 2751 수 정렬하기2 ############
+# N = int(input())
+# arr = [int(input()) for i in range(N)]
+
+## 오답 1 시간 초과 ##
+# arr.sort()
+
+# for i in arr:
+#     print(i)
+
+## sol 1 merge_sort ##
+
+# def merge_sort(array):
+#     if(len(array) <= 1):
+#         return array
+#     mid = len(array) // 2
+#     left = merge_sort(array[: mid])
+#     right = merge_sort(array[mid:])
+
+#     i, j, k = 0, 0, 0
+
+#     while i < len(left) and j < len(right):
+#         if left[i] < right[j]:
+#             array[k] = left[i]
+#             i += 1
+#         else:
+#             array[k] = right[j]
+#             j += 1
+#         k += 1
+
+#     if i == len(left):
+#         while j < len(right):
+#             array[k] = right[j]
+#             j += 1
+#             k += 1
+#     elif j == len(right):
+#         while i < len(left):
+#             array[k] = left[i]
+#             i += 1
+#             k += 1
+
+#     return array
+
+# arr = merge_sort(arr)
+
+# for i in arr:
+#     print(i)
+
+## sol 2 pypy3 sorted ##
+# N = int(input())
+# arr = [int(input()) for i in range(N)]
+
+# for i in sorted(arr):
+#     print(i)
+
+## sol 3 pypy3 sort ##
 # N = int(input())
 # arr = [int(input()) for i in range(N)]
 # arr.sort()
@@ -7,10 +62,22 @@
 #     print(i)
 
 
-# 11650 좌표 정렬하기
-# 11651 좌표 정렬하기2
-# 10814 나이순 정렬
-# 10825 국영수
-# 10989 수 정렬하기3
-# 11652 카드
-# 11004 K번째 수
+############ 11650 좌표 정렬하기 ############
+n = int(input())
+
+corrd = []
+
+for i in range(n):
+    [a, b] = map(int, input().split())
+    corrd.append([a, b])
+
+ans = sorted(corrd)
+for i in range(n):
+    print(ans[i][0], ans[i][1])
+
+############ 11651 좌표 정렬하기2 ############
+############# 10814 나이순 정렬 ############
+############# 10825 국영수 ############
+############# 10989 수 정렬하기3 ############
+############# 11652 카드 ############
+############# 11004 K번째 수 ############
