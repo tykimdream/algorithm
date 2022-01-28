8d3619b2fc2bf0b8a3cb383a9ecdfb45921cb677
tykimdream@korea.ac.kr <tykimdream@korea.ac.kr>
2022-01-28 20:08:21 +0900

    20220128

--- 스택,큐/1406.py
+++ 스택,큐/1406.py
@@ -46,3 +46,26 @@
 # print("결과", init)
 
 # 단순히 푸는 것이 아닌 스택으로 풀어야함
+
+import sys
+
+st1 = list(sys.stdin.readline().rstrip())
+st2 = []
+
+for _ in range(int(sys.stdin.readline())):
+    command = list(sys.stdin.readline().split())
+    if command[0] == 'L':
+        if st1:
+            st2.append(st1.pop())
+    elif command[0] == 'D':
+        if st2:
+            st1.append(st2.pop())
+    elif command[0] == 'B':
+        if st1:
+            st1.pop()
+
+    else:
+        st1.append(command[1])
+
+st1.extend(reversed(st2))
+print(''.join(st1))
