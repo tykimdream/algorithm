a73452bdf844e239ac6522121fd8196baae0538a
tykimdream@korea.ac.kr <tykimdream@korea.ac.kr>
2022-02-04 13:32:53 +0900

    20220204

--- test.py
+++ test.py
@@ -1,23 +1,25 @@
 # 4358
 
-from collections import defaultdict
-import sys
+# from collections import defaultdict
+# import sys
 
-trees = defaultdict(int)
-n = 0
+# trees = defaultdict(int)
+# n = 0
 
-for line in sys.stdin:
-    if line == '\n':
-        break
-    tree = line.rstrip()
-    trees[tree] += 1
-    n += 1
+# for line in sys.stdin:
+#     if line == '\n':
+#         break
+#     tree = line.rstrip()
+#     trees[tree] += 1
+#     n += 1
 
-tree_lst = list(trees.keys())
-tree_lst.sort()
+# tree_lst = list(trees.keys())
+# tree_lst.sort()
 
-for tree in tree_lst:
-    print('%s %.4f' % (tree, trees[tree]/n*100))
+# for tree in tree_lst:
+#     print('%s %.4f' % (tree, trees[tree]/n*100))
 
 
-# test
+a = input()
+
+print(ord(a))
---
+++ 자료구조/10824.py
@@ -0,0 +1,10 @@
+# 22.02.04 10824
+
+import sys
+input = sys.stdin.readline
+
+a, b, c, d = map(str, input().split())
+
+x = a + b
+y = c + d
+print(int(x)+int(y))
