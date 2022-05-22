98fb5a94d3716403244c3627bd1758342a6a7534
tykimdream <tykimdream@gmail.com>
2022-05-22 13:53:08 +0900

    move repo

--- Practice/숫자포맷.py
+++ Practice/숫자포맷.py
@@ -13,3 +13,14 @@
 # for i in range(2+1):
 #     print(i)
 
+n = int(input())
+print()
+arr = []
+for _ in range(n):
+    data = input().split()
+    arr.append((data[0], int(data[1])))
+
+array = sorted(arr, key=lambda student: student[1])
+
+for x in array:
+    print(x[0], end = ' ')
